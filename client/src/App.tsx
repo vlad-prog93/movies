d523720c4d69b8d19b1659265f803392e6d74024
Doroshenko Vladislav <vladka_@bk.ru>
2023-04-10 16:59:29 +0300

    верстаем профиль

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ const App = () => {
   return (
     <Routes>
       <Route path={ROUTES.home} element={<Layout><Home /></Layout>} />
-      <Route path={ROUTES.profile} element={<Layout><Profile /></Layout>} />
+      <Route path={ROUTES.profile} element={<Profile />} />
       <Route path={ROUTES.movies} element={<Layout><Movies /></Layout>} />
       <Route path={ROUTES.savedMovies} element={<Layout><SavedMovies /></Layout>} />
       <Route path={ROUTES.register} element={<Auth />} />
--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,9 +1,43 @@
+// node_modules
+import { useState } from 'react'
+
+// styles
+import Header from 'components/Header'
 import './Profile.css'
 
-
 const Profile = () => {
+  const [isEdit, setIsEdit] = useState<boolean>(false)
+
   return (
-    <h1>Profile</h1>
+    <>
+      <Header />
+      <section className='profile'>
+        <h2 className='profile__title'>Привет, Владислав!</h2>
+        <form className='profile__form'>
+          <label className='profile__label' htmlFor='profile-name'>Имя</label>
+          <input
+            id='profile-name'
+            className='profile__input'
+            type='text'
+            disabled={isEdit}
+            value={'Владислав'} />
+          {isEdit && <div className='profile__line' />}
+          <label className='profile__label' htmlFor='profile-email'>E-mail</label>
+          <input
+            id='profile-email'
+            className='profile__input'
+            type='email'
+            disabled={isEdit}
+            value={'pochta@yandex.ru'} />
+          <div className='profile__button-con'>
+            <button type='button' className='profile__button-edit' onClick={() => setIsEdit(prev => !prev)}>
+              {isEdit ? 'Редактировать' : 'Сохранить'}
+            </button>
+            <button className='profile__button-exit'>Выйти из аккаунта</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
